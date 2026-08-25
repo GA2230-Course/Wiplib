aed41e502479fced9e465d90950c57e30bb66b64
lavy <deckelbaumlavy@gmail.com>
2026-08-25 14:31:35 +0300

    added a stataMachine for the intake class, and used it in toggleIntake command

--- src/main/java/frc/robot/commands/ToggleIntake.java
+++ src/main/java/frc/robot/commands/ToggleIntake.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Intake.SystemState;
 
 public class ToggleIntake extends Command{
     private Intake intake;
@@ -14,10 +15,10 @@ public class ToggleIntake extends Command{
     @Override
     public void initialize() {
         if (intake.isOpen()) {
-            intake.close();
+            intake.setWantedState(SystemState.CLOSE);
             return;
         }
-        intake.open();
+        intake.setWantedState(SystemState.OPEN);
     }
 
     @Override
